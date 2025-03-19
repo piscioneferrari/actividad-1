866cea04d73987feee7649ef147664f6bbe4ca7e
piscioneferrari <piscioneferrari@gmail.com>
2025-03-19 10:25:14 -0300

    Add files via upload

---
+++ piscioneferrari/2.py
@@ -0,0 +1,8 @@
+personas= {
+"personas1":{"nombre": "juan", "edad": "30", "ciudad": "madrid"},
+"personas2":{"nombre": "maria", "edad": "28", "ciudad": "barcelona"}
+}
+print (personas["personas1"])
+datos= personas ["personas1"]
+print (datos)
+print (datos ["edad"])
---
+++ piscioneferrari/sisi.py
@@ -0,0 +1,7 @@
+listas=[1, 3, 50, 31]
+listas.append (8)
+print(listas)
+# borrar un elemento
+del listas [1]
+# modificar el valor de un elemento
+listas [0]= 3
